d96c5fc161260cde975b4fe4365794761367d9a6
kowshik3333 <kowshikbayya1234@gmail.com>
2025-12-01 12:54:20 +0530

    Initial Commit

---
+++ grid/ContentView.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  grid
+//
+//  Created by rentamac on 01.12.25.
+//
+
+import SwiftUI
+ 
+struct ContentView: View {
+    var body: some View {
+        Grid(horizontalSpacing: 16, verticalSpacing: 16) {
+            GridRow {
+                Rectangle()
+                    .fill(Color.white)
+                    .frame(width: 167.5, height: 143.5)
+                    .overlay(
+                        Rectangle()
+                            .stroke(Color.gray, lineWidth: 1)
+                    )
+                Rectangle()
+                    .fill(Color.white)
+                    .frame(width: 167.5, height: 143.5)
+                    .overlay(
+                        Rectangle()
+                            .stroke(Color.gray, lineWidth: 1)
+                    )
+            }
+            GridRow {
+                Rectangle()
+                    .fill(Color.white)
+                    .frame(width: 167.5, height: 143.5)
+                    .overlay(
+                        Rectangle()
+                            .stroke(Color.gray, lineWidth: 1)
+                    )
+                Rectangle()
+                    .fill(Color.white)
+                    .frame(width: 167.5, height: 143.5)
+                    .overlay(
+                        Rectangle()
+                            .stroke(Color.gray, lineWidth: 1)
+                    )
+            }
+        }
+        .padding(16)
+    }
+}
+ 
+struct GridLayout_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ grid/gridApp.swift
@@ -0,0 +1,17 @@
+//
+//  gridApp.swift
+//  grid
+//
+//  Created by rentamac on 01.12.25.
+//
+
+import SwiftUI
+
+@main
+struct gridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
